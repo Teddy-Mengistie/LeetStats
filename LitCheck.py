9f2348647d3d99db5714433cf46a760abe11523e
Teddy M <55628361+Teddy449@users.noreply.github.com>
2020-05-28 17:27:40 -0400

    v2.19

--- LitCheck.py
+++ LitCheck.py
@@ -110,13 +110,14 @@ async def remove(ctx, user):
 @client.command(name = "board")
 async def leaderboard(ctx):
         all = collection.find().sort("week", -1)
-        board = "```{:^76}\n{:^26}{:^25}{:^25}\n".format("***LEADERBOARD***","users", "prob's done", "total")
+        board = "```{:^80}\n{:^26}{:^25}{:^25}\n".format("***LEADERBOARD***","users", "prob's done", "total")
         c = 0;
         for x in all:
-            board += "{}){:^25}{}{:^25}{}{:^25}\n".format(c+1, x["_id"], ":", x["week"], ":", x["problems"] + x["week"])
+            board += "{:>4}){:^25}{}{:^25}{}{:^25}\n".format(c+1, x["_id"], ":", x["week"], ":", x["problems"] + x["week"])
             c+=1
         board+="```"
         await ctx.channel.send(board)
+
 
 @client.command(name = "clrl")
 @commands.has_role("leetcode-manager")
