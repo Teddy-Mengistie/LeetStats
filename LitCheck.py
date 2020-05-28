afd504f2bffec4f3143988b6c4b5b46b13dac61d
Teddy M <55628361+Teddy449@users.noreply.github.com>
2020-05-28 15:53:15 -0400

    v2.07

--- LitCheck.py
+++ LitCheck.py
@@ -99,10 +99,10 @@ async def remove(ctx, user):
 @client.command(name = "board")
 async def leaderboard(ctx):
         all = collection.find().sort("week", -1)
-        board = "```{:^75}\n{:>25}{:>25}{:>15}\n".format("***LEADERBOARD***","users", "prob's done", "total")
+        board = "```{:^75}\n{:^25}{:^25}{:^25}\n".format("***LEADERBOARD***","users", "prob's done", "total")
         c = 0;
         for x in all:
-            board += "{}){:>15}{:>10}{:>15}{:>10}{:>15}\n".format(c+1, x["_id"], ":", x["week"], ":", x["problems"] + x["week"])
+            board += "{}){:>15}{:>10}{:>15}{:>10}{:>25}\n".format(c+1, x["_id"], ":", x["week"], ":", x["problems"] + x["week"])
             c+=1
         board+="```"
         await ctx.channel.send(board)
