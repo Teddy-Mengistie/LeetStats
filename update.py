f123fc62bbfd309729baa088220aa656aa3e8868
Teddy M <55628361+Teddy449@users.noreply.github.com>
2020-05-29 00:40:59 -0400

    v2.319

--- update.py
+++ update.py
@@ -16,6 +16,20 @@ import time
 cluster = MongoClient(os.environ['MONGO_CLIENT'])
 collection = cluster["Bot"]["Leetcode Users Data"]
 
+def problems(user_name):
+    my_url = f'http://leetcode.com/{user_name}'
+    page = requests.get(my_url)
+    soup = BeautifulSoup(page.content, 'lxml')
+    #num problems done/1453
+    num_probs = soup.get_text().replace("\n", "")
+    if("/" in num_probs):
+        begin = num_probs.find("Progress")+8
+        end = num_probs.find("Solved Question")
+        ret = num_probs[begin: end].strip()
+        end = ret.find("/")
+        return int(ret[0:end])
+    else:
+        return -1
 
 while(True):
     all = collection.find()
