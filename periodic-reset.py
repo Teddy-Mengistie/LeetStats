1d914541986caead25d49c06ad8a7026c247d7a8
Teddy M <55628361+Teddy449@users.noreply.github.com>
2020-05-31 18:03:12 -0400

    v3.001

--- periodic-reset.py
+++ periodic-reset.py
@@ -6,10 +6,19 @@ cluster = MongoClient(os.environ['MONGO_CLIENT'])
 collection = cluster["Bot"]["Leetcode Users Data"]
 
 while(True):
-    all = collection.find()
-    for x in all:
-        b = x["_id"]
-        collection.update_many({"_id":b},{"$set":{"problems": x["problems"] + x["week"]}})
-        collection.update_many({"_id":b},{"$set":{"week": 0}})
-    print("***************************Reset Successful***************************")
-    time.sleep(os.environ['RESET_DAYS'] * 24 * 60 * 60)# RESET_DAYS is the amt of days to reset in
+    if(os.environ['INSTANT_RESET']):
+        all = collection.find()
+        for x in all:
+            b = x["_id"]
+            collection.update_many({"_id":b},{"$set":{"problems": x["problems"] + x["week"]}})
+            collection.update_many({"_id":b},{"$set":{"week": 0}})
+        print("***************************Reset Successful***************************")
+        time.sleep(os.environ['RESET_DAYS'] * 24 * 60 * 60)# RESET_DAYS is the amt of days to reset in
+    else:
+        time.sleep(os.environ['RESET_DAYS'] * 24 * 60 * 60)
+        all = collection.find()
+        for x in all:
+            b = x["_id"]
+            collection.update_many({"_id":b},{"$set":{"problems": x["problems"] + x["week"]}})
+            collection.update_many({"_id":b},{"$set":{"week": 0}})
+        print("***************************Reset Successful***************************")
